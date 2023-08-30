d8b208ab962e5704a3ad1b38ce1ecf44b222f8bf
Muhammad Salihu <muhammad.salihu1@yahoo.com>
2023-08-30 13:23:47 +0100

    productize-yourself

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
+import { ChakraProvider } from '@chakra-ui/react'
+
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Welcome to Airbills Digital Solutions
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ChakraProvider>
   );
 }
 
